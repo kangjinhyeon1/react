27c2e2c9f7239ebc67ac2a96f175ff7a20803525
강진현 <kangjin627@gmail.com>
2022-10-05 19:34:18 +0900

    [ADD] 벨로퍼트 강의 끝!~

--- react-tutorials/src/App.js
+++ react-tutorials/src/App.js
@@ -1,19 +1,69 @@
 import React, { Component } from 'react';
 import PhoneForm from './Components/PhoneForm';
+import PhoneInfoList from './Components/PhoneInfoList';
 
 class App extends Component {
 
-  id = 0;
+  id = 3;
 
   state = {
-    infromation: [],
+    infromation: [
+      {
+        id: 0,
+        name: '홍길동',
+        Phone: '010-0000-00001'
+      },
+      {
+        id: 1,
+        name: '강진현',
+        Phone: '010-0000-00002'
+      },
+      {
+        id: 2,
+        name: 'jin',
+        Phone: '010-0000-00003'
+      },
+    ],
+    keyword:'',
   }
+
+  handleChange = (e) =>{
+    this.setState({
+      keyword: e.target.value,
+    })
+  }
+
   handleCreate = (data) =>{
     const {infromation} = this.state;
     this.setState({
       infromation: infromation.concat({
-          ...data
+          ...data,
+          id: this.id++,
       })
+    })
+  }
+
+  handleRemove = (id) =>{
+    const {infromation} = this.state;
+    this.setState({
+      infromation: infromation.filter(info => info.id !== id)
+    })
+  }
+
+  handleUpdate = (id, data) => {
+    const {infromation} = this.state;
+    this.setState({
+      infromation: infromation.map(
+        info => {
+          if (info.id === id){
+            return {
+              id,
+              ...data,
+            };
+          }
+          return info;
+        }
+      )
     })
   }
 
@@ -21,7 +71,18 @@ class App extends Component {
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate}/>
-        {JSON.stringify(this.state.infromation)}
+        <input
+          value={this.state.keyword}
+          onChange={this.handleChange}
+          placeholder="검색..."
+        />
+        <PhoneInfoList 
+          data={this.state.infromation.filter(
+            info => info.name.indexOf(this.state.keyword) > -1
+          )}
+          onRemove = {this.handleRemove}
+          onUpdate = {this.handleUpdate}
+        />
       </div>
     );
   }
