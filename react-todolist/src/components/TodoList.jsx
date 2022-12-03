42c18325c85e36ee0d8c154b8d11d14ffe1d0189
강진현 <kangjin627@gmail.com>
2022-12-03 23:34:43 +0900

    [ADD]기능 얼추 만드는중?

--- react-todolist/src/components/TodoList.jsx
+++ react-todolist/src/components/TodoList.jsx
@@ -1,16 +1,38 @@
 import * as S from "./styles";
-
+import { useState } from "react";
+import plus from "../img/plus.svg";
+import complete from "../img/check.svg";
+import deletebtn from "../img/delete.svg";
 
 function TodoList(){
+    const [ph, setPh] = useState("할 일을 입력!!");
+    const changePh  = () =>{
+        setPh("");
+    }
+    const rechangePh = () => {
+        setPh("할 일을 입력");
+    }
     return (
-        <>
             <S.background>
-                <S.TodoFlex>
-                    <S.TodoContainer></S.TodoContainer>
-                </S.TodoFlex>
-            </S.background>
-        </>
-        
+                <S.TodoContainer>
+                    <S.Title>TODOLIST!!</S.Title>
+                    <S.Input placeholder={ph} onFocus={changePh} onBlur={rechangePh}/>
+                    <S.PlusBtn>
+                        <S.Plus src={plus}/>
+                        <S.PlusBar/>
+                    </S.PlusBtn>
+                    <S.Line/>
+                    <S.TodoList>
+                        <S.Todo>
+                            <S.Content>
+                                과학수행준비하기
+                            </S.Content>
+                            <S.Complete src={complete}/>
+                            <S.Delete src={deletebtn}/>
+                        </S.Todo>
+                    </S.TodoList>
+                </S.TodoContainer>
+            </S.background>  
     );
 }
 
--- react-todolist/src/components/styles.js
+++ react-todolist/src/components/styles.js
@@ -1,21 +1,98 @@
 import styled from "styled-components";
-import bgimg from "../img/background.png"
+import bgimg from "../img/background.png";
 
 export const background = styled.div`
     background-image: url(${bgimg});
     background-repeat: no-repeat;
     background-size: cover;
-    height: 937px;
+    height: 100vh;
+    display:flex;
+    justify-content:center;
+    align-items: center;
 `;
 
-
-export const TodoFlex = styled.div`
-    display: flex;
-    justify-content: center;
-`; 
-
 export const TodoContainer = styled.div`
-    width: 300px;
-    height: 600px;
+    width: 550px;
+    height: 750px;
     background-color: #ECE5F4;
+    border-radius: 20px;
+    display:flex;
+    align-items: center;
+    flex-direction: column;
+`;
+
+export const Title = styled.div`
+    color: #D7B0E9;
+    font-size: 50px;
+    margin-top: 30px;
+`;
+
+export const Input = styled.input`
+    width: 380px;
+    height: 40px;
+    border: 4px solid #A778F2;
+    background-color: transparent;
+    border-radius: 10px;
+    outline: none;
+    font-size: 20px;
+    padding-left: 10px;
+    margin-top: 50px;
+    color: #434343;
+`;
+
+export const PlusBtn = styled.div`
+    cursor: pointer;
+`;
+
+export const Plus = styled.img`
+    position: absolute;
+    top: 27%;
+    left: 119vh;
+    padding-left: 3px;
+`;
+
+export const PlusBar = styled.div`
+    position:absolute;
+    width: 3px;
+    height: 48px;
+    background-color: #A778F2;
+    top: 26%;
+    left: 118vh;
+`;
+
+export const Line = styled.div`
+    width: 100%;
+    height: 3px;
+    background-color: #A778F2;
+    opacity: 0.5;
+    margin-top: 30px;
+`;
+
+export const TodoList = styled.div`
+
+`;
+
+export const Todo = styled.div`
+    border: 4px solid #A778F2;
+    background-color: transparent;
+    border-radius: 10px;
+    width: 380px;
+    height: 42px;
+    padding-left: 10px;
+    margin-top: 30px;
+    display: flex;
+    align-items:center;
+`;
+
+export const Content = styled.div`
+    font-size: 20px;
+    color: #434343;
+`;
+
+export const Complete = styled.img`
+    width: 48px;
+`;
+
+export const Delete = styled.img`
+    width: 48px;
 `;
